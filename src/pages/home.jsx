07f6e22af3290905157a56af5f8dd0dcf4152462
CosmicPun <41039@chs.ac.th>
2024-07-03 20:55:21 +0700

    setup_project

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+export default function Home() {
+    return (
+        <>
+          <div>
+            <label htmlFor="">Select Option:</label>
+            <select name="" id=""></select> 
+            <br />
+            <label htmlFor="">Search:</label> 
+            <input type="text" />
+            <br />
+            <label htmlFor="">Budget:</label>
+            <input type="text" />
+            <br />
+            <button>Go</button>
+          </div> 
+
+          <div>
+            <button>Back</button>  
+            <div></div>
+          </div> 
+        </>
+    );
+}
---
+++ src/router.jsx
@@ -0,0 +1,11 @@
+import {Router, createBrowserRouter} from "react-router-dom";
+import  Home  from "./pages/home";
+
+const router = createBrowserRouter([
+    {
+        path: '',
+        element: <Home />
+    }
+])
+
+export default router;
