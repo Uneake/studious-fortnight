748883b459a7cc97ebdd23c0374934737a3ee281
Uneak <kittipop.suwanna@gmail.com>
2024-07-03 23:50:16 +0700

    decorate and justify

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,20 +2,20 @@ export default function Home() {
 
   return (
     <>
-      <div className="w-full flex flex-col gap-3">
-        <div className="w-40">
-          <label for="combo-box" className="outline-red-950">Select Option:</label>
-          <select id="combo-box"></select>
+      <div className="w-dvh h-dvh flex  flex-col  gap-6 items-center bg-red-300">
+        <div className="rounded grid justify-items-center shadow-lg">
+          <label for="combo-box" >Select Option:</label>
+          <select id="combo-box" className="rounded outline-black outline-none w-40 "></select>
         </div>
-        <div className="w-40">
-          <label for="search-field" >Search:</label>
-          <input type="text" id="search-field" className="outline-red-950 outline-none"></input>
+        <div className="grid justify-items-center shadow-lg">
+          <label for="search-field">Search:</label>
+          <input type="text" id="search-field" className="rounded outline-black outline-none w-40 "></input>
         </div>
-        <div className="w-40">
-          <label for="budget-input">Budget:</label>
-          <input type="text" id="budget-input"></input>
+        <div className="rounded grid justify-items-center shadow-lg">
+          <label for="budget-input" >Budget:</label>
+          <input type="text" id="budget-input" className="w-40 rounded outline-black outline-none"></input>
         </div >
-        <button className="w-fit bg-green-400">Go</button>
+        <button className="w-40 h-fit rounded-lg bg-green-400 px-9 py-1 flex justify-center my-4 shadow-lg">Go</button>
       </div>
     </>
   )
