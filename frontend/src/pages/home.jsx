facd31da35c8aa90e995ee41cdd24e04ee7f2efb
CosmicPun <41039@chs.ac.th>
2024-07-03 23:07:44 +0700

    fix box position

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,17 +2,20 @@ export default function Home() {
 
   return (
     <>
-      <div className="w-full">
-        <label for="combo-box">Select Option:</label>
-        <select id="combo-box"></select>
-        <br />
-        <label for="search-field">Search:</label>
-        <input type="text" id="search-field"></input>
-        <br />
-        <label for="budget-input">Budget:</label>
-        <input type="text" id="budget-input"></input>
-        <br />
-        <button id="go-button">Go</button>
+      <div className="w-full flex flex-col gap-3">
+        <div className="w-40">
+          <label for="combo-box" className="outline-red-950">Select Option:</label>
+          <select id="combo-box"></select>
+        </div>
+        <div className="w-40">
+          <label for="search-field" >Search:</label>
+          <input type="text" id="search-field" className="outline-red-950 outline-none"></input>
+        </div>
+        <div className="w-40">
+          <label for="budget-input">Budget:</label>
+          <input type="text" id="budget-input"></input>
+        </div >
+        <button className="w-fit bg-green-400">Go</button>
       </div>
     </>
   )
