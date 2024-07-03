2ba3a66b0cbe3e8a30b51f63ab386276827af3a6
Uneak <kittipop.suwanna@gmail.com>
2024-07-03 22:42:31 +0700

    replace hello to block

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,18 @@
 export default function Home() {
-  
+
   return (
     <>
-      <div className="">
-        hello
+      <div className="w-full">
+        <label for="combo-box">Select Option:</label>
+        <select id="combo-box"></select>
+        <br />
+        <label for="search-field">Search:</label>
+        <input type="text" id="search-field"></input>
+        <br />
+        <label for="budget-input">Budget:</label>
+        <input type="text" id="budget-input"></input>
+        <br />
+        <button id="go-button">Go</button>
       </div>
     </>
   )
